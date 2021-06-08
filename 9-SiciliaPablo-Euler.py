7812825476736a8f8b2b3a5005490de1833ed8ad
PabloSiciliaAndrade <52049994+PabloSiciliaAndrade@users.noreply.github.com>
2021-06-07 19:27:37 -0500

    first commit

---
+++ 9-SiciliaPablo-Euler.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+
+def input_ecuacion():
+    ecuacion = input()
+    return ecuacion
+
+h = float(input("ingrese el valor del paso: "))
+s = float(input("ingrese el valor final: "))
