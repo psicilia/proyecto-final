c57a7f459da3c0eec297a6adb4a0bb80be06a2bf
Pablo Sicilia <psicilia@lcg.unam.mx>
2021-06-13 22:57:26 -0500

    aproximaciones con errores de covergencia, sin documentacion

--- 1-SiciliaPablo-Aprox.py
+++ 1-SiciliaPablo-Aprox.py
@@ -1,4 +1,3 @@
-import decimal
 from sympy import *
 from math import *
 
@@ -8,10 +7,14 @@ listX = [] #lista de X
 ecuacion = input("ingrese el despeje de su ecuacion")
 listX.append(float(input("ingresa un valor: ")))
 decimal = int(input("cantidad de decimales: "))
+
+class NoConverge(Exception):
+    pass
+
 def fun(x):
     result = eval(ecuacion)
-    #result = (cbrt(1-x))
     return result
+    
 
 def error(x,y):
     relativeE = abs((y-x)/y)
@@ -19,13 +22,16 @@ def error(x,y):
 
 i = 1
 listX.append(fun(listX[-1]))
-while error(listX[-1], listX[-2]) > (10**(-decimal)):
-    if i > 3:
-        pverror = error(listX[-2], listX[-3])
-        if pverror < error(listX[-1], listX[-2]):
-            print("no converge")
-    listX.append(fun(listX[-1]))
-    i = i + 1 
-
-print(listX)
-print(" converge a "+ str(decimal) +" decimales en "+ str(round(listX[-1],decimal)) +" con "+ str(i) + " iteraciones" )
+try:
+    while error(listX[-1], listX[-2]) > (10**(-decimal)):
+        listX.append(fun(listX[-1]))
+        i = i + 1
+        if not type(listX[-1]) is float:
+            raise NoConverge
+except ZeroDivisionError:
+    print("no es posible dividir entre 0")
+except NoConverge:
+    print("no converge con el valor de x aproximado")
+else: 
+    print(listX)
+    print(" converge a "+ str(decimal) +" decimales en "+ str(round(listX[-1],decimal)) +" con "+ str(i) + " iteraciones" )
