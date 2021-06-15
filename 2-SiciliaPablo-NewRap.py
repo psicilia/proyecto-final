a46a724c2a15964e3b26167b4db72799e53b934b
Pablo Sicilia <psicilia@lcg.unam.mx>
2021-06-14 21:03:31 -0500

    2 comentado

--- 2-SiciliaPablo-NewRap.py
+++ 2-SiciliaPablo-NewRap.py
@@ -2,75 +2,53 @@ import sympy as sp
 from math import *
 
 x = sp.Symbol('x')
+# definimos x como simbolo
 print("bienvenido al metodo de Newton Rapson")
 
-listCoef = []
+listEsc = []
+# lista de coeficientes 
 listRes = []
+# lista de resultados
 maxCoef = int(input("ingrese la potencia maxima de su ecuacion: "))
+# leemos el valor de la potencia mas alta, para determinar la cantidad de terminos
 i = 0
 eq = ""
+#inicializamos eq 
 while i <= (maxCoef):
-    listCoef.append(float(input("ingrese el coeficiente de X a la potencia " + str(maxCoef-i) + " en decimal: " )))
+    listEsc.append(float(input("ingrese el escalar de X a la potencia " + str(maxCoef-i) + " en decimal: " )))
+    # leemos los valores escalares para cada termino 
     if maxCoef-i == 0:
-        eq = eq + str(listCoef[-1])
+        eq = eq + str(listEsc[-1])
+        # si es el ultimo termino no agregamos x ni un mas al final
     else:
-        eq = eq + str(listCoef[-1]) + (' * (x**')+ str(maxCoef-i) + ") + "
+        eq = eq + str(listEsc[-1]) + (' * (x**')+ str(maxCoef-i) + ") + "
     i = i + 1
 
 print(eq)
+# mostramos la ecuacion final
 y = eval(eq)
 
 def error(a,b):
     relativeE = abs((b-a)/b)
     return relativeE
+    #definimos el calculo del error relativo
     
 derivada = y.diff(x)
+# derivamos y con respecto a x
 
 def siguiente():
     aprox = (x-(y/derivada))
     return aprox.subs(x, (listRes[-1]))
+    # definimos el calculo del siguiente resultado
 
 decimal = int(input("cantidad de decimales: "))
-listRes.append(float(input("ingrese valor inicial")))
+listRes.append(float(input("ingrese valor inicial: ")))
+#leemos la precision y el valor inicial
 listRes.append(siguiente())
-print(listRes)
+# calculamos el resultado calculando con el valor inicial
 i = 1
 while error(listRes[-1], listRes[-2]) > (10**(-decimal)):
+    # si el error de los resultados es mayor al margen seguimos calculando 
     listRes.append(siguiente())
     i = i + 1
-print(listRes)
 print(" converge a "+ str(decimal) +" decimales en "+ str(round(listRes[-1],decimal)) +" con "+ str(i) + " iteraciones" )
-
-"""
-listX = [] #lista de X 
-ecuacion = input("ingrese el despeje de su ecuacion")
-
-listX.append(float(input("ingresa un valor: ")))
-decimal = int(input("cantidad de decimales: "))
-
-class NoConverge(Exception):
-    pass
-
-def fun(x):
-    result = eval(ecuacion)
-    return result
-    
-
-
-
-i = 1
-listX.append(fun(listX[-1]))
-try:
-    while error(listX[-1], listX[-2]) > (10**(-decimal)):
-        listX.append(fun(listX[-1]))
-        i = i + 1
-        if not type(listX[-1]) is float:
-            raise NoConverge
-except ZeroDivisionError:
-    print("no es posible dividir entre 0")
-except NoConverge:
-    print("no converge con el valor de x aproximado")
-else: 
-    print(listX)
-    print(" converge a "+ str(decimal) +" decimales en "+ str(round(listX[-1],decimal)) +" con "+ str(i) + " iteraciones" )
-"""
