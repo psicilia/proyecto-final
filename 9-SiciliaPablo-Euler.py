27e572c6675b11eee9f1a0da545a6b096dd39049
Pablo Sicilia <psicilia@lcg.unam.mx>
2021-06-14 20:19:49 -0500

    euler funcional, sin comentarios

--- 9-SiciliaPablo-Euler.py
+++ 9-SiciliaPablo-Euler.py
@@ -1,9 +1,27 @@
-
+from sympy import *
+from math import *
 import numpy as np
 
-def input_ecuacion():
-    ecuacion = input()
-    return ecuacion
+x, y = symbols('x y')
+ecuacion = input("ingrese su ecuacion: ")
+f = eval(ecuacion)
+listX = []
+listY = []
 
 h = float(input("ingrese el valor del paso: "))
-s = float(input("ingrese el valor final: "))
+listY.append(float(input("ingrese el valor de y correspondiente a x = 0: ")))
+listX.append(0.0)
+s = float(input("ingrese el valor de x que desea: "))
+i = h
+
+
+while i <= s:
+    newY = listY[-1] + h*(f.subs(x, listX[-1]).subs(y, listY[-1]))
+    listY.append(newY)
+    listX.append(i)
+    i = i + h
+print(listY[-1])
+
+
+
+
